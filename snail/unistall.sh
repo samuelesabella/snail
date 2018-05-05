84eb59a72f17068fc73e78655a9c8784f0ae5034
samueleunipi <samueleunipi@gmail.com>
2018-05-05 10:55:49 +0200

    bug fixes

--- snail/unistall.sh
+++ snail/unistall.sh
@@ -1,7 +1,9 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   PROFILE=~/.bashrc
+  sed -i "/# snail/d" $PROFILE
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   PROFILE=~/.bash_profile
+  sed -i ".backup" "/# snail/d" $PROFILE
 fi
 
 sed -i ".backup" "/# snail/d" $PROFILE
